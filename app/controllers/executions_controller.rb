da091c9040e6cc3fe085ed01f4552f80478ea85d
dsusa122 <d.susa122@uniandes.edu.co>
2013-07-26 02:06:49 -0500

    fix one parameter of execution

--- app/controllers/executions_controller.rb
+++ app/controllers/executions_controller.rb
@@ -195,7 +195,6 @@ class ExecutionsController < InheritedResources::Base
     @execution = Execution.new(params[:execution])
     @execution.user_id = current_user.id
     @application = @execution.application
-    @num_jobs = 1
     @execution.time_per_job = @application.estimated_time
     @execution.vm_type = @application.vm_type
     puts 'CREATE EXECUTION'
@@ -224,6 +223,7 @@ class ExecutionsController < InheritedResources::Base
   def new
 
     @execution = Execution.new
+    @num_jobs = 1
     @execution.user_id = current_user.id
 
     #@uploader.success_action_redirect = cloud_files_url
